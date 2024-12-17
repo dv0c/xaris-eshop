3d1fad17a54752efb683c99f6ef99419e23c2a9d
dv0c <tasosmeidanis12@gmail.com>
2024-12-17 17:38:47 +0200

    modified:   sanity.types.ts 	modified:   src/app/(store)/page.tsx 	new file:   src/components/ui/ProductsView.tsx 	new file:   src/sanity/lib/products/getAllCategories.ts

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,15 +1,18 @@
 import { Button } from "@/components/ui/button";
+import ProductsView from "@/components/ui/ProductsView";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 
 export default async function Home() {
   const products = await getAllProducts();
+  const categories = await getAllCategories()
 
   return (
     <div>
       hello world
       {/* render all products */}
       <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
-        <ProductsView products={products} />
+        <ProductsView categories={categories} products={products} />
       </div>
     </div>
   );
