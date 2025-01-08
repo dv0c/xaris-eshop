0672e3d6cfca1613263dc96fde00d599d8a19ed7
dv0c <tasosmeidanis12@gmail.com>
2025-01-08 20:00:29 +0200

    modified:   app/(storefront)/products/[name]/page.tsx 	modified:   app/components/storefront/CategorySelection.tsx

--- app/(storefront)/products/[name]/page.tsx
+++ app/(storefront)/products/[name]/page.tsx
@@ -4,90 +4,28 @@ import { notFound } from "next/navigation";
 import { unstable_noStore as noStore } from "next/cache";
 
 async function getData(productCategory: string) {
-  switch (productCategory) {
-    case "all": {
-      const data = await prisma.product.findMany({
-        select: {
-          name: true,
-          images: true,
-          price: true,
-          id: true,
-          description: true,
-        },
-        where: {
-          status: "published",
-        },
-      });
+  const data = await prisma.product.findMany({
+    select: {
+      name: true,
+      images: true,
+      price: true,
+      id: true,
+      description: true,
+    },
+    where: {
+      status: "published",
+      name: productCategory,
+    },
+  });
 
-      return {
-        title: "All Products",
-        data: data,
-      };
-    }
-    case "men": {
-      const data = await prisma.product.findMany({
-        where: {
-          status: "published",
-          category: "men",
-        },
-        select: {
-          name: true,
-          images: true,
-          price: true,
-          id: true,
-          description: true,
-        },
-      });
-
-      return {
-        title: "Products for Men",
-        data: data,
-      };
-    }
-    case "women": {
-      const data = await prisma.product.findMany({
-        where: {
-          status: "published",
-          category: "women",
-        },
-        select: {
-          name: true,
-          images: true,
-          price: true,
-          id: true,
-          description: true,
-        },
-      });
-
-      return {
-        title: "Products to Women",
-        data: data,
-      };
-    }
-    case "kids": {
-      const data = await prisma.product.findMany({
-        where: {
-          status: "published",
-          category: "kids",
-        },
-        select: {
-          name: true,
-          images: true,
-          price: true,
-          id: true,
-          description: true,
-        },
-      });
-
-      return {
-        title: "Products for Kids",
-        data: data,
-      };
-    }
-    default: {
-      return notFound();
-    }
+  if (!data) {
+    return notFound();
   }
+
+  return {
+    title: "All Products",
+    data: data,
+  };
 }
 
 export default async function CategoriesPage({
--- app/components/storefront/CategorySelection.tsx
+++ app/components/storefront/CategorySelection.tsx
@@ -1,10 +1,11 @@
+'use client'
+import useCategories from "@/app/lib/hooks/useCategories";
+import all from "@/public/all.jpeg";
 import Image from "next/image";
 import Link from "next/link";
-import all from "@/public/all.jpeg";
-import men from "@/public/men.jpeg";
-import women from "@/public/women.jpeg";
 
 export function CategoriesSelection() {
+  const { categories, error, loading } = useCategories()
   return (
     <div className="py-24 sm:py-32">
       <div className="flex justify-between items-center">
@@ -20,51 +21,27 @@ export function CategoriesSelection() {
         </Link>
       </div>
 
-      <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
-        <div className="group aspect-w-2 aspect-h-1 rounded-xl overflow-hidden sm:aspect-w-1 sm:row-span-2">
-          <Image
-            src={all}
-            alt="All Products Image"
-            className="object-cover object-center "
-          />
-          <div className="bg-gradient-to-b from-transparent to-black opacity-55" />
-          <div className="p-6 flex items-end">
-            <Link href="/products/all">
-              <h3 className="text-white font-semibold">All Products</h3>
-              <p className="mt-1 text-sm text-white">Shop Now</p>
-            </Link>
-          </div>
-        </div>
+      <div className="mt-6 grid grid-cols-1 gap-y-6">
 
-        <div className="group aspect-w-2 aspect-h-1 rounded-lg overflow-hidden sm:relative sm:aspect-none sm:h-full">
-          <Image
-            src={men}
-            alt="Products for men Image"
-            className="object-bottom object-cover sm:absolute sm:inset-0 sm:w-full sm:h-full"
-          />
-          <div className="bg-gradient-to-b from-transparent to-black opacity-55 sm:absolute sm:inset-0" />
-          <div className="p-6 flex items-end sm:absolute sm:inset-0">
-            <Link href="/products/men">
-              <h3 className="text-white font-semibold">Products for Men</h3>
-              <p className="mt-1 text-sm text-white">Shop Now</p>
-            </Link>
-          </div>
-        </div>
+        {categories.map((category) => (
+          <Link href={'/products/' + category.name} key={category.id} className="group aspect-w-3 aspect-h-1 rounded-xl overflow-hidden">
+            <Image
+              src={category?.image}
+              alt={category.name}
+              className="object-cover object-center"
+              fill
+            />
+            <div className="bg-gradient-to-b from-transparent to-black opacity-55" />
+            <div className="p-6 flex items-end">
+              <Link href="/products/all">
+                <h3 className="text-white font-semibold">{category.title}</h3>
+                <p className="mt-1 text-sm text-white">Shop Now</p>
+              </Link>
+            </div>
+          </Link>
+        ))}
 
-        <div className="group aspect-w-2 aspect-h-1 rounded-lg overflow-hidden sm:relative sm:aspect-none sm:h-full">
-          <Image
-            src={women}
-            alt="Women product image"
-            className="object-bottom object-cover sm:absolute sm:inset-0 sm:w-full sm:h-full"
-          />
-          <div className="bg-gradient-to-b from-transparent to-black opacity-55 sm:absolute sm:inset-0" />
-          <div className="p-6 flex items-end sm:absolute sm:inset-0">
-            <Link href="/products/women">
-              <h3 className="text-white font-semibold">Products for Women</h3>
-              <p className="mt-1 text-sm text-white">Shop Now</p>
-            </Link>
-          </div>
-        </div>
+
       </div>
     </div>
   );
